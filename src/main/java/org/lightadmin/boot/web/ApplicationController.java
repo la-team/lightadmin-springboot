3bab2157e247cb5e72778b704f58dea0941893b2
Luigi Polvani <jimmyfm@gmail.com>
2014-09-11 14:49:03 +0200

    Added thymeleaf templating

--- src/main/java/org/lightadmin/boot/web/ApplicationController.java
+++ src/main/java/org/lightadmin/boot/web/ApplicationController.java
@@ -3,18 +3,18 @@ package org.lightadmin.boot.web;
 import org.lightadmin.boot.repository.HotelRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ApplicationController {
 
-    @Autowired
-    private HotelRepository hotelRepository;
+	@Autowired
+	private HotelRepository hotelRepository;
 
-    @RequestMapping("/")
-    @ResponseBody
-    public String helloWorld() {
-        return "Hello hotel: " + hotelRepository.findOne(1l).getName();
-    }
+	@RequestMapping("/")
+	public String thymeleafIndexPage(Model model) {
+		model.addAttribute("hotels", hotelRepository.findAll());
+		return "index";
+	}
 }
